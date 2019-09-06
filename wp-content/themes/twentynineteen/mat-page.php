21aecf1d39453beedac32d8ce1517804778b4f34
Jamie Pham <jtkp66@gmail.com>
2019-09-06 08:40:46 -0700

    adding js to mat-page.php to fetch data from endpoint

---
+++ wp-content/themes/twentynineteen/mat-page.php
@@ -0,0 +1,36 @@
+<?php /* Template Name: Mat Page */
+
+get_header();
+
+?>
+<div class="container">
+<div class="studio-list"></div>
+</div>
+
+<script>
+    const url = 'https://api.myjson.com/bins/1b83qt';
+    const studioContainer = document.querySelector('.studio-list');
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    (console.log(data))
+        data.map( partner => {
+            const content = 
+            `
+            <ul style="list-style-type:none;" class="list-style">
+            <li>
+            <h4><a href="${partner.url}">${partner.location}</a></h4>
+            <p>${partner.studio}</p>
+            ${partner.classdates}
+            </li>
+            </ul>
+            `
+
+            studioContainer.innerHTML += content;
+        })
+    });
+
+</script>
+
+<?php
